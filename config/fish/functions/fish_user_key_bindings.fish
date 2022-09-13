1209f555b4381b5309cbd3e88386bce5083b657e
AlexisMoins <moinsalexis@gmail.com>
2022-09-13 18:50:26 +0200

    feat: added bindings to fish vi mode

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,27 +1,33 @@
 function fish_user_key_bindings
-
     # Bind ^H to history searching via fzf
-    bind \ch fzf-history
     bind --mode insert \ch fzf-history
 
     # ^G for fzf cd
-    bind \cg fzf-git-branch
     bind --mode insert \cg fzf-git-branch
 
     # Bind ^B to find file with fzf
-    bind \cb fzf-find-file
     bind --mode insert \cb fzf-find-file
 
     # Bind ^Z to resume background process
-    bind \cz fg\r
     bind --mode insert \cz fg\r
 
     # Bind ^R to restoring a tmux session
-    bind \cr tmux-restore
     bind --mode insert \cr tmux-restore
 
-    # Bind ^Y to accept suggestion
-    bind \cy forward-char
-    bind --mode insert \cy forward-char
+    function vi-normal-mode
+        if commandline --paging-mode
+            commandline -f cancel
+        else
+            set fish_bind_mode default
+            commandline -f backward-char repaint-mode
+        end
+    end
 
+    bind -M insert \cc vi-normal-mode
+
+    bind -M insert \cy forward-char  # accepts suggestion
+    bind -M insert \ck kill-line
+
+    bind -M insert \cp up-or-search
+    bind -M insert \cn down-or-search
 end
