143ff83de077045ae77cec7e6a3ac3eae2a6b32c
AlexisMoins <moinsalexis@gmail.com>
2022-10-09 10:11:39 +0200

    feat: added fzf key bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,18 +1,17 @@
 function fish_user_key_bindings
-    # Bind ^H to history searching via fzf
-    bind --mode insert \ch fzf-history
+    # ^B -> find file
+    # ^H -> history
+    # ^f -> directory
+    fzf_key_bindings
 
     # ^G for fzf cd
     bind --mode insert \cg fzf-git-branch
-
-    # Bind ^B to find file with fzf
-    bind --mode insert \cb fzf-find-file
 
     # Bind ^Z to resume background process
     bind --mode insert \cz fg\r
 
     # Bind ^R to restoring a tmux session
-    bind --mode insert \cr tmux-restore
+    # bind --mode insert \cr tmux-restore
 
     function vi-normal-mode
         if commandline --paging-mode
@@ -31,3 +30,4 @@ function fish_user_key_bindings
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
 end
+
