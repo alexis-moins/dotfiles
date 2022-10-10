21d999c69a41e0535107421909d75d502789b167
alexis-moins <moinsalexis@gmail.com>
2022-10-10 21:51:41 +0200

    refactor: removed unused fish functions

--- .config/fish/functions/push_that.fish
+++
@@ -1,4 +0,0 @@
-function push_that --argument-names message branch --description 'Stage all changes in the repository then commit using the given message'
-    git add -A && git commit -m "$message"
-    set --query branch && git push origin $branch || git push origin
-end
