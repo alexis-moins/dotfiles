2d001612b7859a67ac092744b55177dac6b67d95
alexis-moins <moinsalexis@gmail.com>
2022-10-14 18:35:38 +0200

    feat: changed telescope mappings

--- .config/nvim/lua/config/packages/telescope.lua
+++ .config/nvim/lua/config/packages/telescope.lua
@@ -1,45 +1,28 @@
 -- Telescope
 local telescope = require('telescope')
-local extensions = telescope.extensions
-
 local telescope_builtin = require('telescope.builtin')
-local telescope_actions = require('telescope.actions')
+
+-- Extensions endpoint
+local extensions = telescope.extensions
 
 -- Setup
 telescope.setup({
-    defaults = {
-        mappings = {
-
-            -- Insert mode mappings
-            i = {
-                ["<C-j>"] = telescope_actions.move_selection_next,
-                ["<C-k>"] = telescope_actions.move_selection_previous,
-                ["<C-u>"] = false
-            },
-
-            -- Normal mode mappings
-            n = {
-                ["<C-j>"] = telescope_actions.move_selection_next,
-                ["<C-k>"] = telescope_actions.move_selection_previous,
-                ["<C-u>"] = false
-            }
-        }
-    },
-
     pickers = {
         find_files = {
             find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--hidden" },
             theme = 'dropdown',
             previewer = false,
         },
+
     },
 
     extensions = {
         file_browser = {
             theme = "ivy",
-            hijack_netrw = true,
+            path = "%:p:h",
 
             hidden = true,
+            hijack_netrw = true,
             respect_gitignore = true,
 
         },
@@ -57,13 +40,14 @@ vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = "Grep fi
 vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = "Find help pages" })
 vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>fe', telescope_builtin.command_history, { desc = "Find commands (history)" })
+vim.keymap.set('n', '<leader>fr', telescope_builtin.oldfiles, { desc = "Find recent files" })
 
 -- Git
 -- Checkout branches
 vim.keymap.set('n', '<leader>fB', telescope_builtin.git_branches, { desc = "Find git branches" })
 -- Commits | Commits from the current branch
 vim.keymap.set('n', '<leader>fc', telescope_builtin.git_commits, { desc = "Find git commits" })
-vim.keymap.set('n', '<leader>fC', telescope_builtin.git_bcommits)
+vim.keymap.set('n', '<leader>fC', telescope_builtin.git_bcommits, { desc = "Find current branch's commits" })
 
 -- File browser extension
 vim.keymap.set('n', '<leader>f.', extensions.file_browser.file_browser, { desc = "Browse files" })
--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -74,3 +74,10 @@ require('packer').startup(function(use)
     end
 
 end)
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer.lua source <afile> | PackerCompile
+  augroup end
+]])
