11c39db67015ffadfe2ad5721210bb88a9950e32
alexis-moins <moinsalexis@gmail.com>
2022-12-17 09:28:25 +0100

    refactor: changed architecture

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,21 +7,4 @@ require 'alexis.mappings'
 
 -- Plugin configuration
 require 'alexis.packer'
-require 'alexis.plugin.lualine'
-
-require 'alexis.plugin.lsp'
-require 'alexis.plugin.debug'
-
-require 'alexis.plugin.cmp'
-require 'alexis.plugin.snippets'
-
-require 'alexis.plugin.telescope'
-require 'alexis.plugin.treesitter'
-
-require 'alexis.plugin.dressing'
-require 'alexis.plugin.gitsigns'
-
-require 'alexis.plugin.which-key'
-require 'alexis.plugin.surround'
-
 require 'nvim-autopairs'.setup()
