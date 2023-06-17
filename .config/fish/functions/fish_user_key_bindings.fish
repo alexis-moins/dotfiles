433cd83194cefb40680ca364ba63fe5a13091b24
alexis-moins <sinus.nacre0v@icloud.com>
2023-06-17 19:18:17 +0200

    feat(fish): added gum file widget for fish shell

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -28,5 +28,7 @@ function fish_user_key_bindings
 
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
+
+    bind -M insert \cb gum-file-widget
 end
 
---
+++ .config/fish/functions/gum-file-widget.fish
@@ -0,0 +1,7 @@
+function gum-file-widget
+    set -l CANDIDATES (fd --strip-cwd-prefix --hidden --follow --type='f' | gum filter --placeholder="Select a file...")
+
+    test -z $CANDIDATES && return 0
+
+    commandline -it -- $CANDIDATES
+end
