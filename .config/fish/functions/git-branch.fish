105da976d45c7b711d78ce9a40705448fab18158
alexis-moins <sinus.nacre0v@icloud.com>
2023-07-10 18:10:28 +0200

    feat(fish): updated gum-find-widget and git-branch

--- .config/fish/functions/git-branch.fish
+++ .config/fish/functions/git-branch.fish
@@ -1,6 +1,6 @@
 function git-branch --description 'Switch git branches interactively'
     set -l branch (git branch --sort='-committerdate' --format='%(refname:short)' \
-        | gum filter --placeholder='Branch to switch to...')
+        | gum filter --placeholder='Branch to switch to...' --no-limit | tr '\n' ' ')
 
     commandline -it -- $branch
 end
--- .config/fish/functions/gum-find-widget.fish
+++ .config/fish/functions/gum-find-widget.fish
@@ -1,7 +1,7 @@
 function gum-find-widget
-    set -l CANDIDATES (fd --strip-cwd-prefix --hidden --follow | gum filter --fuzzy --placeholder="Select a file...")
+    set -l CANDIDATES (fd --strip-cwd-prefix --hidden --follow | \
+        gum filter --fuzzy --placeholder="Select a file..." --no-limit | tr '\n' ' ')
 
     test -z $CANDIDATES && return 0
-
     commandline -it -- $CANDIDATES
 end
