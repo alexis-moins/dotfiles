042a2ed07432202ed879c2cded869125ba7757e2
alexis-moins <sinus.nacre0v@icloud.com>
2023-08-05 16:58:45 +0200

    feat(cheat): small additions

--- .config/fish/functions/cheat.fish
+++ .config/fish/functions/cheat.fish
@@ -1,6 +1,14 @@
-function cheat
-    set -l namespace (curl -s cht.sh/:list | gum filter)
-    set -l query (gum input --placeholder 'query' | tr ' ' '+')
+function cheat --description 'cheat sheets using cht.sh'
+    # Options for a better spin look
+    set SPIN_OPTS --title 'Loading...' --spinner 'minidot' --spinner.foreground '2' --show-output
 
-    gum spin --title 'Loading...' --spinner 'minidot' --spinner.foreground '2' --show-output -- curl "cht.sh/$namespace/$query?style=monokai" | bat
+    set namespaces (gum spin $SPIN_OPTS -- curl 'cht.sh/:list')
+
+    set namespace (echo $namespaces | tr ' ' '\n' | gum filter)
+    test -z $namespace && return 1
+
+    set query (gum input --placeholder 'your query' | tr ' ' '+')
+    test -z $query && return 1
+
+    gum spin $SPIN_OPTS -- curl -s "cht.sh/$namespace/$query?style=monokai" | bat --plain
 end
