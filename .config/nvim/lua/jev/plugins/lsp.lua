b3e59616c4ef4a92ca72187866ea68c5f92d206e
alexis-moins <sinus.nacre0v@icloud.com>
2023-10-25 19:45:10 +0200

    feat: tmux prompt design

--- .config/nvim/lua/jev/plugins/lsp.lua
+++ .config/nvim/lua/jev/plugins/lsp.lua
@@ -134,48 +134,10 @@ return {
 
         })
 
-        -- vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})]])
-
         vim.keymap.set("n", "H", function()
             vim.diagnostic.open_float(nil, { focus = false })
         end, {
             desc = "Open diagnostics popup",
-        })
-
-
-        local namespace = vim.api.nvim_create_namespace("class_conceal")
-        local group = vim.api.nvim_create_augroup("class_conceal", { clear = true })
-
-        local conceal_html_class = function(bufnr)
-            local language_tree = vim.treesitter.get_parser(bufnr, "html")
-            local syntax_tree = language_tree:parse()
-            local root = syntax_tree[1]:root()
-
-            local query = vim.treesitter.query.parse(
-                "html",
-                [[
-    ((attribute
-        (attribute_name) @att_name (#eq? @att_name "class")
-        (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
-    ]]
-            ) -- using single character for conceal thanks to u/Rafat913
-
-            for _, captures, metadata in query:iter_matches(root, bufnr, root:start(), root:end_(), {}) do
-                local start_row, start_col, end_row, end_col = captures[2]:range()
-                vim.api.nvim_buf_set_extmark(bufnr, namespace, start_row, start_col, {
-                    end_line = end_row,
-                    end_col = end_col,
-                    conceal = metadata[2].conceal,
-                })
-            end
-        end
-
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged", "InsertLeave" }, {
-            group = group,
-            pattern = "*.html",
-            callback = function()
-                conceal_html_class(vim.api.nvim_get_current_buf())
-            end,
         })
     end,
 }
